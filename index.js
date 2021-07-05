14407b0039d03f5d0c145470383e31219b3a20f1
Jai <47060357+jdadonut@users.noreply.github.com>
2021-07-04 20:02:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Plugin } = require('powercord/entities');
 
 module.exports = class Template extends Plugin {
     startPlugin() {
+        require('electron').ipcRenderer.removeListener('DISCORD_NAVIGATE_BACK', require('electron').ipcRenderer._events.DISCORD_NAVIGATE_BACK)
         return;
     }
     pluginWillUnload() {
