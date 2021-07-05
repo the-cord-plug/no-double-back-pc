c82d3958f16377a6e9e7dc7eeb0aaeea97e6c74d
Jai <47060357+jdadonut@users.noreply.github.com>
2021-07-04 20:02:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Plugin } = require('powercord/entities');
 
-module.exports = class Template extends Plugin {
+module.exports = class NoDoubleBack extends Plugin {
     startPlugin() {
         require('electron').ipcRenderer.removeListener('DISCORD_NAVIGATE_BACK', require('electron').ipcRenderer._events.DISCORD_NAVIGATE_BACK)
         return;
